19dd4348c4fce5b607de02268107445d679a621d
ku-dmitry <akinc@yandex.ru>
2019-04-22 20:56:39 +0300

    Скрипт ручной генерации маски вписан в интерфейс

--- server/pasticciere.py
+++ server/pasticciere.py
@@ -1,8 +1,16 @@
 from tkinter import *
+import subprocess
 
 # Начало отрисовки интерфейса
 window=Tk()
 window.config(bg="white")
+
+
+def mask(event):
+    cmd = "python ~/Repos/MT.Pasticciere/server/polzunok_threshold.py"
+    PIPE = subprocess.PIPE
+    p = subprocess.Popen(cmd, shell = True)
+
 
 lname = Label(window, text= "Номер устройства")
 lname.grid(row=0, column=0)
@@ -14,6 +22,14 @@ ref = Button(text="Обновить")
 ref.grid(row=0, column=3)
 cl_v = Label(window, text="Версия клиента")
 cl_v.grid(row=0, column=4)
+home = Button(text="Домой")
+home.grid(row=0, column=5)
+camshot = Button(text="Снимок")
+camshot.grid(row=0, column=6)
+selfmask = Button(text="Маска")
+selfmask.bind("<Button-1>", mask)
+selfmask.grid(row=0, column=7)
+
 
 window.title('MT.Pasticciere ver 0.01 (Beshenaya Lopata)')
 window.geometry("600x400+10+10")
