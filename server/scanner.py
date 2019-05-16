7813d86053d6f0ec0a4c4838fc63de7611493c45
Borisov Maxim <bedlamzd@gmail.com>
2019-05-16 17:39:29 +0300

    переписал под обработку с видео

--- server/scanner.py
+++ server/scanner.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-from open3d import *  # only for visuals
+# from open3d import *  # only for visuals
 import time
 import os
 
@@ -9,6 +9,7 @@ Y_0 = -56.1
 Z_0 = 18.6
 
 IMG_PATH = './img/'
+VID_PATH = './path/to/video.mov'
 
 Kz = 0.45
 Kx = 0.303
@@ -48,7 +49,6 @@ def get_img(path):
     low_bound = np.array([0, 0, 240])
     up_bound = np.array([255, 30, 255])
     mask = cv2.inRange(hsv, low_bound, up_bound)
-
     # for x in range(mask.shape[1]):
     #     for y in range(mask.shape[0]):
     #         if (mask.item(y, x) == 255) and (mask.item(y + 1, x) == 255):
@@ -56,13 +56,21 @@ def get_img(path):
     return mask
 
 
+def get_mask(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    low_bound = np.array([0, 0, 240])
+    up_bound = np.array([255, 30, 255])
+    mask = cv2.inRange(hsv, low_bound, up_bound)
+    return mask
+
+
 start = time.time()
 
-Y1 = 0
-p = 0
+Y1 = 0  # счётчик кадров
+p = 0  # счётчик точек
 
 X, Y, Z = 0, 1, 2
-
+"""
 files = os.listdir(IMG_PATH)
 files.sort()
 
@@ -93,6 +101,38 @@ for file in files:
         p += 1
     Y1 += 1
     print('%03d/%03d processed for %03d sec' % (Y1, len(files), time.time() - start))
+"""
+cap = cv2.VideoCapture(VID_PATH)  # чтение видео
+propId_frameCount = 7
+frameCount = cap.get(propId_frameCount)  # всего кадров в файле
+
+num_pnts = (Xend - Xnull) * frameCount  # количество точек в облаке
+ply = np.zeros((num_pnts, 3))  # массив облака точек
+zero_lvl = 0  # нулевой уровень в пикселях
+
+while (cap.isOpened()):
+    ret, frame = cap.read()
+    img = get_mask(frame)
+    if Y1 == 0:
+        zero_lvl = find_z_zero_lvl(img)
+    for x in range(Xnull, Xend):
+        for y in range(zero_lvl + 1, img.shape[0]):
+            if frame.item(y, x):
+                # new_row = np.array([(x - Xnull) * Kx + X_0, Y1 * Ky + Y_0, (y - zero_lvl) * Kz + Z_0])
+                # ply = np.append(ply, [new_row], axis=0)
+                ply[p, X] = (x - Xnull) * Kx + X_0
+                ply[p, Y] = Y1 * Ky + Y_0
+                ply[p, Z] = (y - zero_lvl) * Kz + Z_0
+                break
+        else:
+            # new_row = np.array([(x - Xnull) * Kx + X_0, Y1 * Ky + Y_0, Z_0])
+            # ply = np.append(ply, [new_row], axis=0)
+            ply[p, X] = (x - Xnull) * Kx + X_0
+            ply[p, Y] = Y1 * Ky + Y_0
+            ply[p, Z] = Z_0
+        p += 1
+    Y1 += 1
+    print('%03d/%03d processed for %03d sec' % (Y1, frameCount, time.time() - start))
 time_passed = time.time() - start
 print(time_passed)
 generate_PLY(ply)
