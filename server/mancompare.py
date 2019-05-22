f4b46a3e173b68de2d4071420df4935d0c074adb
unknown <antonmajorov1@gmail.com>
2019-05-22 15:28:57 +0300

    Изменил фото печенек,завернул оба скрипта в функции mancompare и manmask

--- server/mancompare.py
+++ server/mancompare.py
@@ -1,45 +1,60 @@
 import numpy as np
 import cv2
 
-counter = 0
+def mancompare():
+    counter = 0
 
-mask = cv2.imread("mask.png", 0)
-photo = cv2.imread("cookie_changed.jpg", 1)
+    mask = cv2.imread("mask.png", 0)
+    photo = cv2.imread("cookie_changed.png", 1)
+    photo = cv2.imread("2.png", 1)
 
-rows,cols = mask.shape
-print(rows,cols)
+    rows,cols = mask.shape
+    print(rows,cols)
 
-threshlevel_read = open('threshlevel.txt', 'r')
-threshlevel = threshlevel_read.read()
-compairing_result = open('mancompare.txt', 'w')
+    threshlevel_read = open('threshlevel.txt', 'r')
+    threshlevel = threshlevel_read.read()
+    compairing_result = open('compairing_result.txt', 'w')
 
-gray = cv2.cvtColor(photo, cv2.COLOR_BGR2GRAY)
-ret,thresh = cv2.threshold(gray,int(threshlevel),255,cv2.THRESH_BINARY)
-kernel1 = np.ones((4,4),np.uint8)
-opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel1)
-median = cv2.medianBlur(opening,7)
-mask_from_photo = median
+    photo = cv2.resize(photo,None,fx=0.8, fy=0.8, interpolation =  cv2.INTER_CUBIC)
+    gray = cv2.cvtColor(photo, cv2.COLOR_BGR2GRAY)
+    blur = cv2.bilateralFilter(gray, 15, 17, 17)
+    blur = cv2.medianBlur(blur,3)
+    ret,thresh = cv2.threshold(gray,int(threshlevel),255,cv2.THRESH_BINARY)
+    kernel1 = np.ones((3,3),np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel1)
+    median = cv2.medianBlur(opening,9)
+    mask_from_photo = median
 
-cv2.imshow("mask", mask)
-cv2.imshow("photo", median)
+    cv2.imshow("mask", mask)
+    cv2.imshow("mask_from_photo", mask_from_photo)
 
-ret = cv2.matchShapes(mask,mask_from_photo,1,0.0)
+    ret = cv2.matchShapes(mask,mask_from_photo,1,0.0)
 
-for i in range(rows):
-    for j in range(cols):
-        if mask[i,j] != mask_from_photo[i,j]:
-            counter+=1
+    for i in range(rows):
+        for j in range(cols):
+            if mask[i,j] != mask_from_photo[i,j]:
+                counter+=1
+                photo[i,j] = [0,0,255]
+            if (mask[i,j] == mask_from_photo[i,j]) & (mask[i,j] == 255):
+                photo[i,j] = [0,255,0]
 
-print(counter)
 
-if ret < 0.01:
-    answer = "yes"
-else:
-    answer = "no"
 
-cv2.waitKey(0)
 
-compairing_result.write(answer)
-cv2.destroyAllWindows()
-threshlevel_read.close()
-compairing_result.close()
+    cv2.imshow("photo", photo)
+    print(counter)
+    print(ret)
+
+    if ret < 0.001:
+        answer = "yes"
+    else:
+        answer = "no"
+
+    cv2.waitKey(0)
+
+    compairing_result.write(answer)
+    cv2.destroyAllWindows()
+    threshlevel_read.close()
+    compairing_result.close()
+    
+mancompare()
--- server/manmask.py
+++ server/manmask.py
@@ -3,28 +3,31 @@ import cv2
 
 def nothing(x):
      pass
+def manmask():
+    cv2.namedWindow('threshholding')
+    cv2.createTrackbar('Tlevel','threshholding',0,255,nothing)
+    while True:
+        original = cv2.imread("cookie.png", 1)
+        original = cv2.resize(original,None,fx=0.8, fy=0.8, interpolation =  cv2.INTER_CUBIC)
+        gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+        blur = cv2.bilateralFilter(gray, 15, 17, 17)
+        blur = cv2.medianBlur(blur,3)
+        threshlevel = cv2.getTrackbarPos('Tlevel','threshholding')
+        ret,thresh = cv2.threshold(gray,threshlevel,255,cv2.THRESH_BINARY)
+        kernel1 = np.ones((3,3),np.uint8)
+        opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel1)
+        median = cv2.medianBlur(opening,9)
+        cv2.imshow("blur", blur)
+        #cv2.imshow("threshholding", opening)
+        cv2.imshow("gray", gray)
+        cv2.imshow("threshholding", median)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-cv2.namedWindow('threshholding')
-cv2.createTrackbar('Tlevel','threshholding',0,255,nothing)
-while True:
-    original = cv2.imread("cookie.jpg", 1)
-    gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
-    blur = cv2.bilateralFilter(gray, 15, 17, 17)
-    blur = cv2.medianBlur(blur,3)
-    threshlevel = cv2.getTrackbarPos('Tlevel','threshholding')
-    ret,thresh = cv2.threshold(gray,threshlevel,255,cv2.THRESH_BINARY)
-    kernel1 = np.ones((3,3),np.uint8)
-    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel1)
-    median = cv2.medianBlur(opening,7)
-    cv2.imshow("original", blur)
-    #cv2.imshow("threshholding", opening)
-    cv2.imshow("gray", gray)
-    cv2.imshow("threshholding", median)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    cv2.destroyAllWindows()
+    cv2.imwrite("mask.png", median)
+    threshlevel_save = open('threshlevel.txt', 'w')
+    threshlevel_save.write(str(threshlevel))
+    threshlevel_save.close()
 
-cv2.destroyAllWindows()
-cv2.imwrite("mask.png", median)
-threshlevel_save = open('threshlevel.txt', 'w')
-threshlevel_save.write(str(threshlevel))
-threshlevel_save.close()
+manmask()
