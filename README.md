541d86a67e39e181bea38670d2afa16ec4846590
unknown <antonmajorov1@gmail.com>
2019-05-30 17:57:13 +0300

    Добавил алгоритм распознавания печеньки на столе для ОТК.

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 * opencv-python
 * ezdxf
 * pygcode
+* imutils
 
 В папке server находятся скрипты для запуска на стороне сервера.
 В папке client лежат клиентские программы.
--- server/otk.py
+++ server/otk.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import configparser
+import imutils
 
 path = "settings.ini"
 
@@ -30,23 +31,110 @@ def nothing(x):
 
 
 def manmask():
+    original = cv2.imread("photo_1.jpg",1)
+    #вырезаю область со столом
+    table = original[13:700, 260:1000]
+    #вырезаю область со столом
+    gray_table = cv2.cvtColor(table, cv2.COLOR_BGR2GRAY)
+    #выделяю персиковый цвет стола по хуевилу
+    lower_color = np.array([133,20,0])
+    upper_color = np.array([215,155,255])
+    hsv = cv2.cvtColor(table, cv2.COLOR_BGR2HSV)
+    only_table = cv2.inRange(hsv,lower_color,upper_color)
+    only_table = cv2.bitwise_not(only_table)
+    # убираем шумы
+    blur = cv2.medianBlur(only_table, 7)
+    kernel = np.ones((5,5),np.uint8)
+    opening = cv2.morphologyEx(blur,cv2.MORPH_OPEN,kernel, iterations = 4)
+    only_table = opening
+    # выделяем область, которая точно является задним фоном
+    sure_bg = cv2.dilate(opening,kernel,iterations=3)
+    # находим область, которая точно является печеньками
+    dist_transform = cv2.distanceTransform(only_table,cv2.DIST_L2,3)
+    ret, sure_fg = cv2.threshold(dist_transform,0.1*dist_transform.max(),255,0)
+    # Finding unknown region
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg,sure_fg)
+
+    # Marker labelling
+    ret, markers = cv2.connectedComponents(sure_fg)
+    # Add one to all labels so that sure background is not 0, but 1
+    markers = markers+1
+    # Now, mark the region of unknown with zero
+    markers[unknown==255] = 0
+    markers = cv2.watershed(table,markers)
+
+
+
+    blank_space = np.zeros(gray_table.shape, dtype="uint8")
+    #blank_space[markers == -1] = 255
+    blank_space[markers == 1] = 255
+    blank_space = cv2.bitwise_not(blank_space)
+
+
+    height, width = blank_space.shape
+    blank_space_cropped = blank_space[2:height-2, 2:width-2]
+
+    contours = cv2.findContours(blank_space_cropped.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    contours = imutils.grab_contours(contours)
+    contours = sorted(contours, key = cv2.contourArea, reverse = True)[:1]
+    table = cv2.bitwise_and(table,table,mask = blank_space)
+    table = cv2.bitwise_and(table,table,mask = sure_bg)
+
+    for cnt in contours:
+
+            #cv2.drawContours(table, [cnt], -1, (0, 255, 255), 2)
+
+            rect = cv2.minAreaRect(cnt)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            #cv2.drawContours(table,[box],0,(0,0,255),2)
+            mult = 1
+
+            W = rect[1][0]
+            H = rect[1][1]
+
+            Xs = [i[0] for i in box]
+            Ys = [i[1] for i in box]
+            x1 = min(Xs)
+            x2 = max(Xs)
+            y1 = min(Ys)
+            y2 = max(Ys)
+
+            rotated = False
+            angle = rect[2]
+
+            if angle < -45:
+                angle+=90
+                rotated = True
+
+            center = (int((x1+x2)/2), int((y1+y2)/2))
+            size = (int(mult*(x2-x1)),int(mult*(y2-y1)))
+            #cv2.circle(table, center, 10, (0,255,0), -1) #again this was mostly for debugging purposes
+
+            M = cv2.getRotationMatrix2D((size[0]/2, size[1]/2), angle, 1.0)
+
+            cropped = cv2.getRectSubPix(table, size, center)
+            cropped = cv2.warpAffine(cropped, M, size)
+
+            croppedW = W if not rotated else H
+            croppedH = H if not rotated else W
+
+            croppedRotated = cv2.getRectSubPix(cropped, (int(croppedW*mult), int(croppedH*mult)), (size[0]/2, size[1]/2))
+
+
     cv2.namedWindow('threshholding')
     cv2.createTrackbar('Tlevel', 'threshholding', 0, 255, nothing)
+    gray = cv2.cvtColor(croppedRotated, cv2.COLOR_BGR2GRAY)
+    blur = cv2.bilateralFilter(gray, 15, 17, 17)
+    blur = cv2.medianBlur(blur, 3)
+
     while True:
-        original = cv2.imread("cookie.png", 1)
-        original = cv2.resize(original, None, fx=0.8, fy=0.8,
-                              interpolation=cv2.INTER_CUBIC)
-        gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
-        blur = cv2.bilateralFilter(gray, 15, 17, 17)
-        blur = cv2.medianBlur(blur, 3)
         threshlevel = cv2.getTrackbarPos('Tlevel', 'threshholding')
         ret, thresh = cv2.threshold(gray, threshlevel, 255, cv2.THRESH_BINARY)
         kernel1 = np.ones((3, 3), np.uint8)
         opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel1)
         median = cv2.medianBlur(opening, 9)
-        cv2.imshow("blur", blur)
-        # cv2.imshow("threshholding", opening)
-        cv2.imshow("gray", gray)
         cv2.imshow("threshholding", median)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -54,7 +142,6 @@ def manmask():
     cv2.destroyAllWindows()
     cv2.imwrite("mask.png", median)
     update_setting(path, "OTK", "threshlevel", str(threshlevel))
-
 
 def mancompare(threshlevel):
     counter = 0
