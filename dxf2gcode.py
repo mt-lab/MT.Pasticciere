eb1f3e9b7c2879062a5ad8b9fe2645824a750acb
Borisov Maxim <bedlamzd@gmail.com>
2019-06-12 22:04:26 +0300

    разделил dxf2gcode() на меньшие функции с определенной задачей

--- dxf2gcode.py
+++ dxf2gcode.py
@@ -10,54 +10,75 @@ def dxf_reader(dxf, modelspace, elements_heap=[]):  # at first input is modelspa
         if element.dxftype() == 'INSERT':
             block = dxf.blocks[element.dxf.name]
             dxf_reader(dxf, block, elements_heap)
+        elif element_redef(element):
+            elements_heap.append(element_redef(element))
         else:
-            elements_heap.append(element)
+            print('empty element')
     return elements_heap
 
 
-def dxf2gcode(pathToDxf, pathToPLY, offset=(0, 0)):
-    PATH_TO_DXF = pathToDxf
-    cloud = pathToPLY
-    dxf = ez.readfile(PATH_TO_DXF)
-    msp = dxf.modelspace()
-    # get all entities from dxf
-    entities = dxf_reader(dxf, msp)
-    # read pcd
-    pcd, pcd_xy, pcd_z = read_pcd(cloud)
+def element_redef(element):
+    if element.dxftype() == 'POLYLINE':
+        return Polyline(element)
+    elif element.dxftype() == 'SPLINE':
+        return Spline(element)
+    elif element.dxftype() == 'LINE':
+        return Line(element)
+    elif element.dxftype() == 'CIRCLE':
+        return Circle(element)
+    elif element.dxftype() == 'ARC':
+        return Arc(element)
+    elif element.dxftype() == 'ELLIPSE':
+        pass
+    elif element.dxftype() == 'LWPOLYLINE':
+        pass
+    elif element.dxftype() == 'POINT':
+        pass
+    # else:
+    #     return Element(element)
 
-    # for easier work with elements define them with elements class
-    unproc = []
-    for e in entities:
-        if e.dxftype() == 'POLYLINE':
-            unproc.append(Polyline(e))
-        elif e.dxftype() == 'SPLINE':
-            unproc.append(Spline(e))
-        elif e.dxftype() == 'LINE':
-            unproc.append(Line(e))
-        elif e.dxftype() == 'CIRCLE':
-            unproc.append(Circle(e))
-        elif e.dxftype() == 'ARC':
-            unproc.append(Arc(e))
 
-    # organize path
+def organize_path(elements, start_point=(0, 0)):
     path = []
-    unproc.sort(key=lambda x: x.best_distance((0, 0)))
-    while len(unproc) != 0:
-        cur = unproc[0]
+    elements.sort(key=lambda x: x.best_distance(start_point))
+    while len(elements) != 0:
+        cur = elements[0]
         path.append(cur)
-        unproc.pop(0)
-        unproc.sort(key=lambda x: x.best_distance(cur.get_points()[-1]))
+        elements.pop(0)
+        elements.sort(key=lambda x: x.best_distance(cur.get_points()[-1]))
+    return path
 
+
+def process_path(path, offset=(0, 0), pathToPLY=PCD_PATH):
+    # read pcd
+    pcd, pcd_xy, pcd_z = read_pcd(pathToPLY)
     # slice dxf and add volume to it, also add offset
     for element in path:
         element.slice(step)
         element.set_offset(offset)
         element.add_z(pcd_xy, pcd_z)
 
-    # generate gcode
-    gcode = gcode_generator(path)
 
-    # write gcode
-    with open('cookie.gcode', 'w+') as gfile:
-        for line in gcode:
-            gfile.write(line + '\n')
+def write_gcode(gcode_instructions, filename='cookie.gcode'):
+    with open(filename, 'w+') as gcode:
+        for line in gcode_instructions:
+            gcode.write(line + '\n')
+
+
+def dxf2gcode(pathToDxf=DXF_PATH, pathToPLY=PCD_PATH, offset=(0, 0)):
+    dxf = ez.readfile(pathToDxf)
+    msp = dxf.modelspace()
+    # get all entities from dxf
+    elements_heap = dxf_reader(dxf, msp)
+
+    # organize path
+    path = organize_path(elements_heap)
+
+    # slice dxf and add volume to it, also add offset
+    process_path(path, offset, pathToPLY)
+
+    # generate gcode instructions as array
+    gcode_instructions = gcode_generator(path)
+
+    # write gcode to text file
+    write_gcode(gcode_instructions)
