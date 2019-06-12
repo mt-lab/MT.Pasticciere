e44d7984d9e8b70970f827582caa674487c7188b
Borisov Maxim <bedlamzd@gmail.com>
2019-06-12 17:44:00 +0300

    добавил взятие значений из settings.ini

--- global_variables.py
+++ global_variables.py
@@ -1,4 +1,40 @@
+import configparser
+
+
+def get_config(path):
+    """
+    Выбираем файл настроек
+    """
+    config = configparser.ConfigParser()
+    config.read(path)
+    return config
+
+
+def update_setting(path, section, setting, value):
+    """
+    Обновляем параметр в настройках
+    """
+    config = get_config(path)
+    config.set(section, setting, value)
+    with open(path, "w") as config_file:
+        config.write(config_file)
+
+
+def get_setting(path, section, setting):
+    """
+    Выводим значение из настроек
+    """
+    config = get_config(path)
+    value = config.get(section, setting)
+    msg = "{section} {setting} is {value}".format(
+        section=section, setting=setting, value=value
+    )
+    print(msg)
+    return value
+
+config_path = 'settings.ini'
 X, Y, Z = 0, 1, 2
-error = 0.2
+accuracy = float(get_setting(config_path, 'GCoder', 'accuracy'))
+step = float(get_setting(config_path, 'GCoder', 'slice_step'))
 Z_max = 30
-step = 1
+
