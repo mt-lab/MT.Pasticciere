75f9d04b13d22786bbf0e1bbcbc2879ea7cab8e1
Maxim Borisov <bedlamzd@gmail.com>
2019-06-23 10:59:27 +0300

    рефактор кода

--- dxf2gcode.py
+++ dxf2gcode.py
@@ -1,3 +1,10 @@
+"""
+dxf2gcode.py
+Author: bedlamzd of MT.lab
+
+Чтение .dxf файла, слайсер этого dxf, перевод в трёхмерное пространство на рельеф объектов,
+генерация gcode в соответствующий файл
+"""
 import ezdxf as ez
 from global_variables import *
 from elements import *
@@ -5,19 +12,38 @@ from utilities import *
 from gcode_gen import *
 
 
-def dxf_reader(dxf, modelspace, elements_heap=[]):  # at first input is modelspace
+def dxfReader(dxf, modelspace, elementsHeap=[]):  # at first input is modelspace
+    """
+    Рекуррентная функция
+    Собирает данные об элементах в dxf с заходом во все внутренние блоки. При этом описывая их
+    собственным классом Element для удобства
+
+    :param dxf: исходное векторное изображение
+    :param modelspace: пространство элементов изображения, либо пространство блока с элементами
+    :param elementsHeap: массив в который собираются все элементы
+    :return elements_heap: массив со всеми элементами из dxf
+    """
     for element in modelspace:
+        # если элемент это блок, то пройтись через вложенные в него элементы
         if element.dxftype() == 'INSERT':
             block = dxf.blocks[element.dxf.name]
-            dxf_reader(dxf, block, elements_heap)
-        elif element_redef(element):
-            elements_heap.append(element_redef(element))
+            dxfReader(dxf, block, elementsHeap)
+        # если элемент не блок и при этом переопределяем, то записать его переопределение в массив
+        elif elementRedef(element):
+            elementsHeap.append(elementRedef(element))
+        # если переопределение вернуло пустой элемент (ещё не описанный), то написать об этом
         else:
             print('empty element')
-    return elements_heap
+    return elementsHeap
 
 
-def element_redef(element):
+def elementRedef(element):
+    """
+    Функция для переопределения полученного элемента в соответствующий подкласс класса Element
+
+    :param element: элемент из dxf
+    :return: переопределение этого элемента
+    """
     if element.dxftype() == 'POLYLINE':
         return Polyline(element)
     elif element.dxftype() == 'SPLINE':
@@ -38,20 +64,32 @@ def element_redef(element):
     #     return Element(element)
 
 
-def organize_path(elements, start_point=(0, 0)):
+def organizePath(elements, start_point=(0, 0)):
+    """
+    Сортирует и ориентирует элементы друг за другом относительно данной точки
+
+    :param elements: элементы, которые необходимо сориентировать и сортировать
+    :param start_point: точка, относительно которой выбирается первый элемент
+    :return path: отсортированный и ориентированный массив элементов
+    """
     path = []
+    # сортировать элементы по их удалению от точки
     elements.sort(key=lambda x: x.best_distance(start_point))
     while len(elements) != 0:
-        cur = elements[0]
-        path.append(cur)
+        # первый элемент в списке (ближайший к заданной точке) - текущий
+        current = elements[0]
+        # добавить его в сориентированный массив
+        path.append(current)
+        # убрать этот элемент из неотсортированного списка
         elements.pop(0)
-        elements.sort(key=lambda x: x.best_distance(cur.get_points()[-1]))
+        # отсортировать элементы по их удалению от последней точки предыдущего элемента
+        elements.sort(key=lambda x: x.best_distance(current.get_points()[-1]))
     return path
 
 
-def process_path(path, offset=(0, 0), pathToPLY=PCD_PATH):
+def processPath(path, offset=(0, 0), pathToPly=PCD_PATH):
     # read pcd
-    pcd, pcd_xy, pcd_z = read_pcd(pathToPLY)
+    pcd, pcd_xy, pcd_z = read_pcd(pathToPly)
     # slice dxf and add volume to it, also add offset
     for element in path:
         element.slice(step)
@@ -59,26 +97,26 @@ def process_path(path, offset=(0, 0), pathToPLY=PCD_PATH):
         element.add_z(pcd_xy, pcd_z)
 
 
-def write_gcode(gcode_instructions, filename='cookie.gcode'):
+def writeGcode(gcode_instructions, filename='cookie.gcode'):
     with open(filename, 'w+') as gcode:
         for line in gcode_instructions:
             gcode.write(line + '\n')
 
 
-def dxf2gcode(pathToDxf=DXF_PATH, pathToPLY=PCD_PATH, offset=(0, 0)):
+def dxf2gcode(pathToDxf=DXF_PATH, pathToPly=PCD_PATH, offset=(0, 0)):
     dxf = ez.readfile(pathToDxf)
     msp = dxf.modelspace()
     # get all entities from dxf
-    elements_heap = dxf_reader(dxf, msp)
+    elements_heap = dxfReader(dxf, msp)
 
     # organize path
-    path = organize_path(elements_heap)
+    path = organizePath(elements_heap)
 
     # slice dxf and add volume to it, also add offset
-    process_path(path, offset, pathToPLY)
+    processPath(path, offset, pathToPly)
 
     # generate gcode instructions as array
     gcode_instructions = gcode_generator(path)
 
     # write gcode to text file
-    write_gcode(gcode_instructions)
+    writeGcode(gcode_instructions)
