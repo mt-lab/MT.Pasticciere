e3c81b030247de7ccb71f217982e52459ef2f8e2
ku-dmitry <akinc@yandex.ru>
2019-06-24 14:25:22 +0300

    Поправил выдачу лога с учётом нескольких модулей

--- pasticciere.py
+++ pasticciere.py
@@ -21,7 +21,7 @@ import time
 logger = logging.getLogger("pasticciere")
 logger.setLevel(logging.INFO)
 fh = logging.FileHandler("pasticciere.log")
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 logger.info("Запуск программы")
