5872af22bf32390204af67bf8c2d1c02153db343
Borisov Maxim <bedlamzd@gmail.com>
2019-06-25 15:02:11 +0300

    закомментил костыль чтобы всё работало

--- dxf2gcode.py
+++ dxf2gcode.py
@@ -10,10 +10,37 @@ from global_variables import *
 from elements import *
 from utilities import *
 from scanner import *
-from gcode_gen import *
+from pygcode import *
 
-cookies = findCookies()[0]
-cookieCenter = cookies[0][0][::-1]
+# cookies = findCookies()[0]
+# cookieCenter = cookies[0][0][::-1]
+
+Z_up = Z_max + 3  # later should be cloud Z max + few mm сейчас это глобальный максимум печати принтера по Z
+extrusionCoefficient = 0.41
+
+
+# when path is a set of elements
+def gcode_generator(path, preGcode=[], postGcode=[]):
+    # TODO: префиксный и постфиксный Gcode
+    # TODO: генерация кода по печенькам
+    gcode = []
+    last_point = (0, 0, 0)
+    E = 0
+    gcode.append('G28')
+    for count, element in enumerate(path, 1):
+        way = element.getSlicedPoints()
+        gcode.append(f'; {count:3d} element')
+        if distance(last_point, way[0]) > accuracy:
+            gcode.append(str(GCodeRapidMove(Z=Z_up)))
+            gcode.append(str(GCodeRapidMove(X=way[0][X], Y=way[0][Y])))
+            gcode.append(str(GCodeRapidMove(Z=way[0][Z])))
+            last_point = way[0]
+        for point in way[1:]:
+            E += round(extrusionCoefficient * distance(last_point, point), 3)
+            gcode.append(str(GCodeLinearMove(X=point[X], Y=point[Y], Z=point[Z])) + f' E{E:3.3f}')
+            last_point = point
+        last_point = way[-1]
+    return gcode
 
 def dxfReader(dxf, modelspace, elementsHeap=[]):  # at first input is modelspace
     """
