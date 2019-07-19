122494a0d0642e41b525c14531e5ea524e94d179
Borisov Maxim <bedlamzd@gmail.com>
2019-07-19 22:46:08 +0300

    мелочи для более удобной обработки в будущем

--- cookie.py
+++ cookie.py
@@ -1,8 +1,9 @@
 class Cookie:
-    def __init__(self, center=(0, 0), width=0, length=0, height=0, rotation=0):
+    def __init__(self, center=(0, 0), width=0, length=0, centerHeight=0, rotation=0, maxHeight=0):
         self.center = center  # мм
         self.width = width  # мм
         self.length = length  # мм
-        self.height = height # мм
+        self.centerHeight = centerHeight  # мм
+        self.maxHeight = maxHeight  # мм
         self.rotation = rotation  # угол
-        self.drawing = None  # dxf или ещё один класс под рисунок, предположительно не нужно
+        self.drawing = None  # dxf или ещё один класс под рисунок или набор точек рисунка (sliced) соответствующий ей
