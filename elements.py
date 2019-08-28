5ef18994dfbf80d5fc8f8e082a1da92b8800f465
bedlamzd <bedlamzd@gmail.com>
2019-08-28 20:26:00 +0300

    перенёс классы из drawing в elements и создал новый класс Contours

--- elements.py
+++ elements.py
@@ -9,7 +9,8 @@ Author: bedlamzd of MT.lab
 import ezdxf as ez
 import numpy as np
 from utilities import X, Y, Z, pairwise, diap, findPointInCloud, distance
-from math import sqrt, cos, sin, pi
+from numpy import sqrt, cos, sin, pi
+from configValues import accuracy
 
 
 class Element:
@@ -75,11 +76,13 @@ class Element:
         """
         return self.sliced if not self.backwards else self.sliced[::-1]
 
-    def getLength(self):
+    def calculateLength(self):
         """
         Рассчитать длину элемента
         """
-        pass
+        for p1, p2 in pairwise(self.sliced):
+            self.length += distance(p1, p2)
+        return self.length
 
     def slice(self, step=1):
         """
@@ -107,7 +110,7 @@ class Element:
         #       весами сделать расстояние до соседей
         if len(self.sliced) != 0:
             for p in self.sliced:
-                p[Z] = findPointInCloud(p, pcd_xy, pcd_z,pcd)
+                p[Z] = findPointInCloud(p, pcd_xy, pcd_z, pcd)
         elif len(self.points) != 0:
             for p in self.points:
                 p.append(findPointInCloud(p, pcd_xy, pcd_z, pcd))
@@ -129,13 +132,7 @@ class Polyline(Element):
         self.first = self.points[0]
         self.last = self.points[-1]
         self.sliced = []
-        self.length = self.getLength()
-
-    def getLength(self):
-        length = 0
-        for p1, p2 in pairwise(self.points):
-            length += distance(p1, p2)
-        return length
+        self.length = 0
 
 
 class LWPolyline(Polyline):
@@ -168,10 +165,7 @@ class Line(Element):
         self.first = self.points[0]
         self.last = self.points[-1]
         self.sliced = []
-        self.length = self.getLength()
-
-    def getLength(self):
-        return distance(self.first, self.last)
+        self.length = 0
 
 
 class Circle(Element):
@@ -191,10 +185,7 @@ class Circle(Element):
             self.center[X] + self.radius * cos(self.endAngle), self.center[Y] + self.radius * sin(self.endAngle))
         self.points = [self.first, self.last]
         self.sliced = []
-        self.length = self.getLength()
-
-    def getLength(self):
-        return (self.endAngle - self.startAngle) * self.radius
+        self.length = 0
 
     def slice(self, step=1):
         angle_step = step / self.radius * (self.endAngle - self.startAngle) / abs(
@@ -227,9 +218,157 @@ class Arc(Circle):
             self.center[X] + self.radius * cos(self.endAngle), self.center[Y] + self.radius * sin(self.endAngle))
         self.points = [self.first, self.last]
         self.sliced = []
-        self.length = self.getLength()
+        self.length = 0
 
 
 class Ellipse(Element):
     # TODO: написать обработку эллипсов
     pass
+
+
+class Contour:
+    def __init__(self):
+        self.elements = []
+        self.n_elements = 0
+        self.length = 0
+
+    def addElement(self, element):
+        self.elements.append(element)
+
+    def length(self):
+        for element in self.elements:
+            self.length += element.length
+        return self.length
+
+
+class Drawing:
+    #TODO: шаблон dxf по которому рисунок делится на слои:
+    #   0 - общий контур печенья, по которому найти центр и поворот рисунка
+    #   1 - самый внешний/важный контур
+    #   ...
+    #   last - элементы для печати в конце
+    def __init__(self, dxf, offset=(0, 0), rotation=0):
+        self.dxf = dxf
+        self.modelspace = self.dxf.modelspace()
+        self.elements = []
+        self.contours = []
+        self.readDxf(self.modelspace)
+        self.center = (0,0)
+        self.offset = offset
+        self.rotation = rotation
+        self.path = ()
+        self.organizePath()
+
+    def readDxf(self, root):
+        for element in root:
+            if element.dxftype() == 'INSERT':
+                block = self.dxf.blocks[element.dxf.name]
+                self.readDxf(block)
+            elif elementRedef(element):
+                self.elements.append(elementRedef(element))
+
+    def slice(self, step=1.0):
+        for element in self.elements:
+            element.slice(step)
+
+    def findCenter(self):
+        """
+        Расчитывает геометрический центр рисунка
+        :return:
+        """
+        pass
+
+    def setOffset(self, offset=(0, 0)):
+        self.offset = offset
+        self.offset()
+
+    def offset(self):
+        for element in self.path:
+            element.setOffset(self.offset)
+
+    def findRotation(self):
+        """
+        Расчитывает поворот рисунка
+        :return:
+        """
+        pass
+
+    def setRotation(self, rotation=0):
+        self.rotation = rotation
+        self.rotate()
+
+    def rotate(self):
+        pass
+
+    def addZ(self):
+        pass
+
+    # def adjustPath(self, offset=(0, 0), pathToPly=PCD_PATH):
+    #     pcd, pcd_xy, pcd_z = readPointCloud(pathToPly)
+    #     # add volume to dxf, also add offset
+    #     for element in self.path:
+    #         element.setOffset(offset)
+    #         element.addZ(pcd_xy, pcd_z, pcd)
+
+    def organizePath(self, start_point=(0, 0)):
+        """
+        Сортирует и ориентирует элементы друг за другом относительно данной точки
+
+        :param elements: элементы, которые необходимо сориентировать и сортировать
+        :param start_point: точка, относительно которой выбирается первый элемент
+        :return path: отсортированный и ориентированный массив элементов
+        """
+        path = []
+        elements = self.elements.copy()
+        # сортировать элементы по их удалению от точки
+        elements.sort(key=lambda x: x.bestDistance(start_point))
+        while len(elements) != 0:
+            # первый элемент в списке (ближайший к заданной точке) - текущий
+            current = elements[0]
+            # добавить его в сориентированный массив
+            path.append(current)
+            # убрать этот элемент из неотсортированного списка
+            elements.pop(0)
+            # отсортировать элементы по их удалению от последней точки предыдущего элемента
+            elements.sort(key=lambda x: x.bestDistance(current.getPoints()[-1]))
+        self.path = path
+
+    def findContours(self):
+        contour = []
+        for e1, e2 in pairwise(self.path):
+            contour.append(e1)
+            d = distance(e1.last, e2.first)
+            if d > accuracy:
+                self.contours.append(contour)
+                contour = [e2]
+            else:
+                contour.append(e2)
+        self.contours.append(contour)
+
+
+def elementRedef(element):
+    """
+    Функция для переопределения полученного элемента в соответствующий подкласс класса Element
+
+    :param element: элемент из dxf
+    :return: переопределение этого элемента
+    """
+    if element.dxftype() == 'POLYLINE':
+        return Polyline(element)
+    elif element.dxftype() == 'SPLINE':
+        return Spline(element)
+    elif element.dxftype() == 'LINE':
+        return Line(element)
+    elif element.dxftype() == 'CIRCLE':
+        return Circle(element)
+    elif element.dxftype() == 'ARC':
+        return Arc(element)
+    elif element.dxftype() == 'ELLIPSE':
+        pass
+    elif element.dxftype() == 'LWPOLYLINE':
+        pass
+    elif element.dxftype() == 'POINT':
+        pass
+    else:
+        print('Unknown element')
+        return None
