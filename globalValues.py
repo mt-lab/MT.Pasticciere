9d6f861a8475de2977b23e6b05c3a836a3b0ee6a
bedlamzd <bedlamzd@gmail.com>
2019-08-29 21:31:47 +0300

    вынес найденные печенья в глобальные переменные

--- globalValues.py
+++ globalValues.py
@@ -3,4 +3,5 @@ from configValues import cameraAngle, cameraHeight
 from utilities import readHeightMap
 
 heightMap = readHeightMap()
+cookies = None
 distanceToLaser = cameraHeight/np.cos(cameraAngle)
