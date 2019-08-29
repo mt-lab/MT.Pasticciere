aaae31cdfb1a38ccc0aa3234ba2afb2f70049166
bedlamzd <bedlamzd@gmail.com>
2019-08-29 21:32:21 +0300

    мелкий рефактор

--- utilities.py
+++ utilities.py
@@ -54,8 +54,13 @@ def avg(*arg):
     return reduce(lambda a, b: a + b, arg) / len(arg)
 
 
-def distance(p1, p2=(0, 0), simple=False):
-    """ Calculate distance between 2 points either 2D or 3D """
+def distance(p1, p2=(.0, .0, .0), simple=False):
+    """
+        Calculate distance between 2 points either 2D or 3D
+        :param tuple of float p1: точка от которой считается расстояние
+        :param tuple of float p2: точка до которой считается расстояние
+        :param bool simple: if True то оценочный расчёт расстояния без использования sqrt
+    """
     p1 = list(p1)
     p2 = list(p2)
     if len(p1) < 3:
@@ -72,6 +77,7 @@ def triangleArea(p1, p2, p3):
     detA = np.linalg.det(A)
     return abs(detA)
 
+
 def insideTriangle(p, a, b, c):
     S = triangleArea(a, b, c)
     Spab = triangleArea(p, a, b)
@@ -81,7 +87,8 @@ def insideTriangle(p, a, b, c):
         return True
     return False
 
-def heightByTrigon(p=(0,0), a=(0,0,0), b=(0,0,0), c=(0,0,0)):
+
+def heightByTrigon(p=(0, 0), a=(0, 0, 0), b=(0, 0, 0), c=(0, 0, 0)):
     axy = np.asarray(a)
     bxy = np.asarray(b)
     cxy = np.asarray(c)
@@ -93,11 +100,12 @@ def heightByTrigon(p=(0,0), a=(0,0,0), b=(0,0,0), c=(0,0,0)):
     S1 = triangleArea(pxy, axy, bxy)
     S2 = triangleArea(pxy, bxy, cxy)
     S3 = triangleArea(pxy, cxy, axy)
-    height = c[Z]*S1/S+a[Z]*S2/S+b[Z]*S3/S
+    height = c[Z] * S1 / S + a[Z] * S2 / S + b[Z] * S3 / S
     return height
 
+
 def apprxPointHeight(point, *arg):
-    if len(point)<2:
+    if len(point) < 2:
         return None
     if len(arg) != 3:
         if len(arg[0]) == 3:
@@ -107,40 +115,40 @@ def apprxPointHeight(point, *arg):
     arg = sorted(arg, key=lambda p: distance(p[:2]))
     det_values = np.zeros(len(arg))
     points = np.asarray(arg)
-    matrix = np.asarray([points[0][:2],points[1][:2],points[2][:2]])
+    matrix = np.asarray([points[0][:2], points[1][:2], points[2][:2]])
     matrix = np.c_[matrix, np.ones(3)]
     area = np.linalg.det(matrix)
     for i, p1 in enumerate(points):
-        j = i +1 if i < len(points)-1 else 0
-        k = i +2 if i < len(points)-2 else i + 1 -(len(points)-1)
+        j = i + 1 if i < len(points) - 1 else 0
+        k = i + 2 if i < len(points) - 2 else i + 1 - (len(points) - 1)
         p2 = points[j]
-        matrix = np.asarray([point[:2],p1[:2], p2[:2]])
+        matrix = np.asarray([point[:2], p1[:2], p2[:2]])
         matrix = np.c_[matrix, np.ones(3)]
         det = np.linalg.det(matrix)
         det_values[k] = det
     if area == det_values.sum():
         det_values = abs(det_values)
-        a_values = det_values/det_values.sum()
-        height = np.multiply(a_values, points[:,2]).sum()
+        a_values = det_values / det_values.sum()
+        height = np.multiply(a_values, points[:, 2]).sum()
         return height
     return None
 
 
-def lineFrom2points(p1=(0,0), p2=(0,0)):
-    k = (p2[X]-p1[X])/(p2[Y]-p1[Y])
-    b = p1[Y]-k*p1[X]
+def lineFrom2points(p1=(0, 0), p2=(0, 0)):
+    k = (p2[X] - p1[X]) / (p2[Y] - p1[Y])
+    b = p1[Y] - k * p1[X]
     return k, b
 
 
-def perpendicular2line(p=(0,0), k=1,b=0):
-    pk = 1/k
-    pb = p[X]/k + p[Y]
+def perpendicular2line(p=(0, 0), k=1, b=0):
+    pk = 1 / k
+    pb = p[X] / k + p[Y]
     return pk, pb
 
 
-def crossectionOfLines(k1 = 1, b1=0, k2=1, b2=0):
-    x = (b2-b1)/(k1-k2)
-    y = k1*x + b1
+def crossectionOfLines(k1=1, b1=0, k2=1, b2=0):
+    x = (b2 - b1) / (k1 - k2)
+    y = k1 * x + b1
     return (x, y)
 
 
@@ -168,7 +176,8 @@ def findPointInCloud(point, pcd_xy, pcd_z, pcd=None):
     # point.append(z if z else 0)
     return z if z else 0
 
-def findAngleOfView(range:'in pxls'=640, focal: 'in mm'=focal, pxlSize: 'in mm'=pxlSize):
+
+def findAngleOfView(range: 'in pxls' = 640, focal: 'in mm' = focal, pxlSize: 'in mm' = pxlSize):
     """
 
     :param range: длинна обзора в пикселях
@@ -176,9 +185,11 @@ def findAngleOfView(range:'in pxls'=640, focal: 'in mm'=focal, pxlSize: 'in mm'=
     :param pxlSize: размер пикселя на матрице
     :return: угол в радианах
     """
-    return 2 * arctan(range*pxlSize/2/focal)
+    return 2 * arctan(range * pxlSize / 2 / focal)
 
-def findCameraAngle(viewWidth:'in mm', frameWidth:'in pxls'=640, cameraHeight:'in mm'=cameraHeight, focal:'in mm'=focal, pxlSize:'in mm'=pxlSize):
+
+def findCameraAngle(viewWidth: 'in mm', frameWidth: 'in pxls' = 640, cameraHeight: 'in mm' = cameraHeight,
+                    focal: 'in mm' = focal, pxlSize: 'in mm' = pxlSize):
     """
 
     :param viewWidth: ширина обзора по центру кадра в мм
@@ -188,19 +199,20 @@ def findCameraAngle(viewWidth:'in mm', frameWidth:'in pxls'=640, cameraHeight:'i
     :param pxlSize: размер пикселя на матрице в мм
     :return: угол наклона камеры в радианах
     """
-    viewAngle = findAngleOfView(frameWidth, focal,pxlSize)
-    cos_cameraAngle = 2*cameraHeight/viewWidth*tan(viewAngle/2)
+    viewAngle = findAngleOfView(frameWidth, focal, pxlSize)
+    cos_cameraAngle = 2 * cameraHeight / viewWidth * tan(viewAngle / 2)
     cameraAngle = arccos(cos_cameraAngle)
     return cameraAngle
 
 
-def saveHeightMap(heightMap:np.ndarray, filename='heightMap.txt'):
+def saveHeightMap(heightMap: np.ndarray, filename='heightMap.txt'):
     with open(filename, 'w') as outfile:
         outfile.write('{0}\n'.format(heightMap.shape))
         outfile.write('# Data starts here\n')
         for dimension in heightMap:
             np.savetxt(outfile, dimension, fmt='%-7.3f')
             outfile.write('# New dimension\n')
+
 
 def readHeightMap(filename='heightMap.txt'):
     if isfile(filename):
