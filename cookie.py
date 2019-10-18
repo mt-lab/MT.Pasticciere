fe345b4cc8d610a8fc7c32e9dff881158b8e162d
Maxim Borisov <bedlamzd@gmail.com>
2019-10-18 18:12:51 +0300

    новые property для упрощения работы с объектом; рефактор

--- cookie.py
+++ cookie.py
@@ -7,11 +7,14 @@ import cv2
 class Cookie:
     def __init__(self, height_map, contour, bounding_box=None):
         self._contour = contour  # контур из целой карты высот
+        self._contour_mm = None
         self._height_map = height_map  # область печеньки на карте высот
         self._center = None  # центр печенья в мм (X, Y, Z)
         self._pixel_center = None  # центр печенья в пикселях на целой карте высот
         self._bounding_box = bounding_box  # центр, высота и ширина области печеньки на карте высот
+        self._bounding_box_mm = None
         self._min_bounding_box = None
+        self._min_bounding_box_mm = None
         self._width = None  # размер печеньки вдоль Y оси в мм
         self._length = None  # размер печеньки вдоль X оси в мм
         self._max_height = None  # максимальная высота в контуре ограничивающем печенье в мм
@@ -26,81 +29,85 @@ class Cookie:
         return self._contour
 
     @property
+    def contour_mm(self):
+        if self._contour_mm is None:
+            col, row, _, _ = self.bounding_box
+            self._contour_mm = np.asarray([(self.height_map[point[0][1] - row, point[0][0] - col, Y],
+                                            self.height_map[point[0][1] - row, point[0][0] - col, X]) for point in
+                                           self.contour],
+                                          dtype=np.float32)
+        return self._contour_mm
+
+    @property
     def center(self):
-        if self._center is not None:
-            return self._center
-        else:
+        if self._center is None:
             self.find_center_and_rotation()
-            return self._center
+        return self._center
 
     @property
     def rotation(self):
-        if self._rotation is not None:
-            return self._rotation
-        else:
+        if self._rotation is None:
             self.find_center_and_rotation()
-            return self._rotation
+        return self._rotation
 
     @property
     def width(self):
-        if self._width is not None:
-            return self._width
-        else:
-            self._width = self.min_bounding_box[1][1]
-            return self._width
+        if self._width is None:
+            self._width = self.min_bounding_box_mm[1][1]
+        return self._width
 
     @property
     def length(self):
-        if self._length is not None:
-            return self._length
-        else:
-            self._length = self.min_bounding_box[1][0]
-            return self._length
+        if self._length is None:
+            self._length = self.min_bounding_box_mm[1][0]
+        return self._length
 
     @property
     def pixel_center(self):
-        if self._pixel_center is not None:
-            return self._pixel_center
-        else:
+        if self._pixel_center is None:
             moments = cv2.moments(self.contour)
             col = int(round(moments['m10'] / moments['m00']))
             row = int(round(moments['m01'] / moments['m00']))
             self._pixel_center = (row, col)
-            return self._pixel_center
+        return self._pixel_center
 
     @property
     def bounding_box(self):
-        if self._bounding_box is not None:
-            return self._bounding_box
-        else:
+        if self._bounding_box is None:
             self._bounding_box = cv2.boundingRect(self.contour)
-            return self._bounding_box
+        return self._bounding_box
+
+    @property
+    def bounding_box_mm(self):
+        if self._bounding_box_mm is None:
+            self._bounding_box_mm = cv2.boundingRect(self.contour_mm)
+        return self._bounding_box_mm
 
     @property
     def min_bounding_box(self):
-        if self._min_bounding_box is not None:
-            return self._min_bounding_box
-        else:
+        if self._min_bounding_box is None:
             self._min_bounding_box = cv2.minAreaRect(self.contour)
-            return self._min_bounding_box
+        return self._min_bounding_box
+
+    @property
+    def min_bounding_box_mm(self):
+        if self._min_bounding_box_mm is None:
+            self._min_bounding_box_mm = cv2.minAreaRect(self.contour_mm)
+        return self._min_bounding_box_mm
 
     @property
     def max_height(self):
-        if self._max_height is not None:
-            return self._max_height
-        else:
+        if self._max_height is None:
             self._max_height = self.height_map[:, :, Z].max()
-            return self._max_height
+        return self._max_height
 
     def find_center_and_rotation(self):
+        # TODO: допилить под нахождение true центра
         center_row, center_col = self.pixel_center
         col, row, w, h = self.bounding_box  # кординаты описывающего прямоугольника
         center_z = self.height_map[center_row - row, center_col - col, Z]
         # Найти центр и поворот контура по точкам в мм
-        tmp = np.asarray([(self.height_map[point[0][1] - row, point[0][0] - col, Y],
-                           self.height_map[point[0][1] - row, point[0][0] - col, X]) for point in self.contour],
-                         dtype=np.float32)
-        moments = cv2.moments(tmp)
+        moments = cv2.moments(self.contour_mm)
         center_x = moments['m10'] / moments['m00']
         center_y = moments['m01'] / moments['m00']
         a = moments['m20'] / moments['m00'] - center_x ** 2
