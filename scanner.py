ef9f9336a1c978aa8be089eccba80b4a8d080a0a
Maxim Borisov <bedlamzd@gmail.com>
2019-12-17 21:26:01 +0300

    переписан способ детекта меток старта; добавлены классы данных Mark и Checkpoint для отдельных меток и последовательности соответственно; теперь при детекте учитываются точки с неподходящими высотами; пригодность метки оценивается на основе сравнения с возможными последовательностями перепадов высот;

--- scanner.py
+++ scanner.py
@@ -6,7 +6,9 @@ Author: bedlamzd of MT.lab
 """
 
 import numpy as np
+from dataclasses import dataclass, field
 from numpy import cos, sin, tan, pi, arctan
+from ezdxf.math.vector import Vector
 import cv2
 from typing import Union, Optional, Tuple
 from utilities import Error, mid_idx, print_objects
@@ -75,6 +77,100 @@ def find_chekpoint(coords: np.ndarray,
             return False, None, None
     marks = np.column_stack((starts_coords, ends_coords, w, np.insert(g, 0, starts_coords[0])))
     return True, marks, np.column_stack((starts, ends))
+
+
+@dataclass
+class Mark:
+    idc: Tuple[int]
+    coords: Tuple
+    height: float = field(init=False)
+    length: float = field(init=False)
+    width: float = field(init=False)
+    rais: float = field(init=False)
+    fall: float = field(init=False)
+
+    def __post_init__(self):
+        self.coords = tuple(Vector(coord) for coord in self.coords)
+        self.height = (self.coords[1].z + self.coords[-2].z) / 2
+        self.length = self.coords[0].xy.distance(self.coords[-1].xy)
+        self.width = self.coords[1].xy.distance(self.coords[-2].xy)
+        self.rais = self.coords[0].xy.distance(self.coords[1].xy)
+        self.fall = self.coords[-2].xy.distance(self.coords[-1].xy)
+
+
+@dataclass
+class Checkpoint:
+    marks: Tuple[Mark]
+    n: int = field(init=False)
+    gaps: Tuple = field(init=False)
+
+    def __post_init__(self):
+        self.n = len(self.marks)
+        self.gaps = tuple(
+            self.marks[i - 1].coords[-1].xy.distance(self.marks[i].coords[0].xy) for i in range(1, self.n))
+
+
+def checker(coords, height, width=None, gaps=None, n: int = None, tol: float = 0.5, *, a: int = 1, b: int = 3,
+            **kwargs) -> (bool, Checkpoint):
+    gap_tol = kwargs.get('gap_tol', 2 * tol)
+    height_tol = kwargs.get('height_tol', tol)
+    width_tol = kwargs.get('width_tol', tol)
+
+    m = {a: ['end'], b: [-b, -(a + b)], a + b: [-b, -(a + b)], -a: [a + b], -b: ['end'], -(a + b): [a]}
+
+    def gaps_check(checkpoint: Checkpoint, gaps, gap_tol):
+        return not gaps or np.all(np.abs(np.array(checkpoint.gaps) - gaps) < gap_tol)
+
+    def width_check(checkpoint: Checkpoint, width, width_tol):
+        return not width or np.all(np.abs(np.array([mark.width for mark in checkpoint.marks]) - width) < width_tol)
+
+    def n_check(checkpoint: Checkpoint, n):
+        return not n or checkpoint.n == n
+
+    def full_check(checkpoint: Checkpoint, gaps, width, n, gap_tol, width_tol):
+        return gaps_check(checkpoint, gaps, gap_tol) and width_check(checkpoint, width, width_tol) and n_check(
+            checkpoint, n)
+
+    def make_sequence(coords, height, height_tol):
+        sequence = np.copy(coords[..., Z])
+        sequence[np.abs(sequence) < height_tol] = 0
+        sequence[(sequence != 0) & (np.abs(sequence - height) > height_tol)] = -a
+        sequence[sequence > 0] = b
+        return np.diff(sequence, prepend=0, append=0)[1:-1]
+
+    def process_sequence(sequence, coords):
+        stack = []
+        idc = []
+        marks = []
+        for idx, item in enumerate(sequence):
+            try:
+                if not m.get(item):
+                    continue
+                elif 'end' in m.get(item) and stack and item in m.get(stack[-1]):
+                    stack.append(item)
+                    idc = idc + [idx, idx] if item == -b else idc + [idx]
+                    if len(idc) == 2:
+                        print()
+                    marks.append(Mark(idc, coords[idc]))
+                    stack = []
+                    idc = []
+                    continue
+            except IndexError:
+                print()
+            if (not stack and item in (-a, b)) or (stack and item in m.get(stack[-1], [])):
+                stack.append(item)
+                idc = idc + [idx, idx] if item == b else idc + [idx]
+            else:
+                stack = []
+                idc = []
+        return marks
+
+    marks = process_sequence(make_sequence(coords, height, height_tol), coords)
+    checkpoint = Checkpoint(marks) if marks else False
+    if checkpoint and full_check(checkpoint, gaps, width, n, gap_tol, width_tol):
+        return True, checkpoint
+    else:
+        return False, None
 
 
 def find_coords(frame_idx: int, laser: np.ndarray, zero_level: np.ndarray,
@@ -384,7 +480,8 @@ def detect_start_img(img, height, width=None, gaps=None, n=None, tol=0.5, roi=No
     laser[(laser > zero_level) | (laser == 0)] = zero_level[(laser > zero_level) | (laser == 0)]
     coords = find_coords(0, laser, zero_level, (img.shape[0], img.shape[1]), mirror, reverse, **kwargs)
     width = None if width is None else width * (tangent ** 2 + 1) ** .5
-    start, pos, idx = find_chekpoint(coords, height, width, gaps, n, tol)
+    # start, pos, idx = find_chekpoint(coords, height, width, gaps, n, tol)
+    start, checkpoint = checker(coords, height, width, gaps, n, tol)
     ####################################################################################################################
     # """ for debug purposes """
     ####################################################################################################################
@@ -394,9 +491,11 @@ def detect_start_img(img, height, width=None, gaps=None, n=None, tol=0.5, roi=No
         img[row_start:row_stop, [col_start + padl, col_stop - padr]] = (127, 255, 127)  # границы поиска zero_lvl
         img[laser.astype(np.int)[col_start:col_stop], np.mgrid[col_start:col_stop]] = (0, 255, 0)  # лазер
         img[zero_level.astype(np.int)[col_start:col_stop], np.mgrid[col_start:col_stop]] = (255, 0, 0)  # zero_lvl
-        if idx is not None:
-            for s, e in idx:  # найденные импульсы
-                img[laser.astype(np.int)[s:e + 1], np.mgrid[s:e + 1]] = (0, 0, 255)
+        if start:
+            for mark in checkpoint.marks:
+                id1, id2, id3, id4 = mark.idc
+                img[laser.astype(np.int)[id1:id4], id1:id4] = (255, 0, 255)
+                img[laser.astype(np.int)[id2:id3], id2:id3] = (0, 0, 255)
     ####################################################################################################################
     return start, img if debug else None, mask if debug else None
 
